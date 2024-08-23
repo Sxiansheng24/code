a4cefe6b0436f97e7ab3565d7626f2706bc80d06
Sxiansheng24 <142140253+Sxiansheng24@users.noreply.github.com>
2024-08-23 10:08:16 +0800

    Update test_stage2.py

--- tools/test_stage2.py
+++ tools/test_stage2.py
@@ -2,14 +2,14 @@ import subprocess
 import os
 import sys
 
-# 定义从.sh脚本中传递的参数
-CONFIG = "./projects/configs/voxformer/voxformer-T.py"
-CHECKPOINT = "/home/ubuntu/VoxFormer/权重/stage2/stage2_T/stage2_T_mIou_5208pre_7844/epoch_11.pth"
-GPUS = 2
-PORT = 29503  # 如果命令行中没有提供PORT，则使用默认值
 
-# 附加传递给test.py的额外参数
-extra_args = sys.argv[1:]  # 获取Python脚本后面的所有参数
+CONFIG = "projects/configs/AEFFSSC/AEFFSSC.py"
+CHECKPOINT = "epoch_11.pth"
+GPUS = 2
+PORT = 29503 
+
+
+extra_args = sys.argv[1:]  
 
 # 设置PYTHONPATH环境变量
 pythonpath_dir = os.path.dirname(os.path.abspath(__file__))
@@ -25,9 +25,9 @@ cmd = [
           CONFIG, CHECKPOINT,
           "--launcher", "pytorch",
           "--eval", "bbox"
-      ] + extra_args  # 添加额外的参数
+      ] + extra_args  
 
-# 执行命令
+
 try:
     subprocess.check_call(cmd)
 except subprocess.CalledProcessError as e:
